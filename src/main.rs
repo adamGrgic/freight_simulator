42909d8f432d44eb7d50b99b9ec7f6d8546ea0b7
adamGrgic <argrgic@gmail.com>
2025-02-03 20:01:37 -0700

    initial thoughts

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+    println!("=_-=_-__-- Freight Simulator --___-_=-_=");
+
+    // zero, we start program
+    // + load Game
+    // first, user enters game information
+    // + name
+    // + character type
+    //
+    // level 1
+    // ----------------------
+    // start as a new business (depending on your character profile)
+    // for the sake of brevity we'll pretend this cycle is for the tech ceo startup that discovered
+    // some new freight technology or logistics platform or some dumb shit so now they're running
+    // the logistical biz
+    //
+    // You start with initial funding
+    // 10 employees
+    // 8 delivery trucks
+    //
+    // To get to the next level you must meet some sort of balance and number of employees
+    //
+    // Level 2
+    // ----------------------
+
+}
